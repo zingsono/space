715ec92f90e69f8a9e0b0539c90886a0022326f4
Zengs <zingsono@gmail.com>
2019-08-28 15:02:38 +0800

    update

--- micro/ms-control/mic/model/ms_user.go
+++ micro/ms-control/mic/model/ms_user.go
@@ -1,1 +1,22 @@
 package model
+
+type (
+	//操作员用户: 用户编号、登录账号、邮箱、手机、密码（MD5）、昵称、头像、角色（数组）、备注、创建时间、创建人、更新时间、更新人
+	//uid,loginId,email,mobile,password,nickname,avatar,roles,remark,createdAt,createdBy,updatedAt,updatedBy
+	MsUser struct {
+		Uid       string   `json:uid`
+		LoginId   string   `json:loginId`
+		Email     string   `json:email`
+		Mobile    string   `json:mobile`
+		Password  string   `json:password`
+		Nickname  string   `json:nickname`
+		Avatar    string   `json:avatar`
+		Roles     []string `json:roles`
+		Remark    string   `json:remark`
+		CreatedAt string   `json:createdAt`
+		CreatedBy string   `json:createdBy`
+		UpdatedAt string   `json:updatedAt`
+		UpdatedBy string   `json:updatedBy`
+	}
+)
+
--- micro/ms-control/mic/router.go
+++ micro/ms-control/mic/router.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+
+func ListenServer() {
+
+	//设置路由规则
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "version 1")
+	})
+
+	//使用默认的DefaultServeMux
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
